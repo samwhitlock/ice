ccee33999852cf42635b376684e1b9f30a7b5f76
Sam Whitlock <phynominal@gmail.com>
2010-05-04 13:36:43 -0700

    added a function to determine the number of bits in a uint32_t

--- bit_twiddle.h
+++ bit_twiddle.h
@@ -6,15 +6,5 @@
 
 inline unsigned int num_bits(uint32_t xor_str)
 {
-    unsigned int total_bits_set; // store the total here
-    static const int S[] = {1, 2, 4, 8, 16}; // Magic Binary Numbers
-    static const int B[] = {0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF, 0x0000FFFF};
-    
-    total_bits_set = xor_str - ((xor_str >> 1) & B[0]);
-    total_bits_set = ((total_bits_set >> S[1]) & B[1]) + (total_bits_set & B[1]);
-    total_bits_set = ((total_bits_set >> S[2]) + total_bits_set) & B[2];
-    total_bits_set = ((total_bits_set >> S[3]) + total_bits_set) & B[3];
-    total_bits_set = ((total_bits_set >> S[4]) + total_bits_set) & B[4];
-    
-    return total_bits_set;
+    return __builtin_popcount((unsigned int) xor_str);
 }
