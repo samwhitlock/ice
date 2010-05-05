3e6ca37db75d89e352c76fd5456ded5f84e7b8ba
Michael Forney <michael@obberon.com>
2010-05-04 22:24:12 -0700

    Fix test_read_pbm

--- tests/test_read_pbm.c
+++ tests/test_read_pbm.c
@@ -4,6 +4,7 @@
  */
 
 #include "ice.h"
+#include "pbm.h"
 #include "test.h"
 
 bool test_61c()
@@ -18,34 +19,34 @@ bool test_61c()
      * 1 1 1 0 1 1 1 0 1 1 1 0 1 0 1 1 1
      */
 
-    int width, height;
-
     uint32_t * state;
 
     const uint32_t expected_state[] = {
-        0x771d1,
-        0x11150,
-        0x17750,
-        0x14550,
-        0x777d1
+        0x1d177,
+        0x05111,
+        0x05771,
+        0x05541,
+        0x1d777
     };
 
-    state = read_pbm("tests/pbm/61c_end.pbm", &state, &width, &height);
+    read_pbm("tests/pbm/61c_end.pbm", &state, &state_width, &state_height);
 
-    if (!states_equal(state, expected_state))
+    state_size = state_height * 4;
+
+    if (state_width != 17 && state_height != 5)
+    {
+        printf("width: %u, expected_width: %u\n", state_width, 17);
+        printf("height: %u, expected_height: %u\n", state_height, 5);
+
+        return false;
+    }
+    else if (!states_equal(state, expected_state))
     {
         puts("state read:");
         print_state(state);
 
         puts("state expected:");
         print_state(expected_state);
-
-        return false;
-    }
-    else if (width != 17 && height != 5)
-    {
-        printf("width: %u, expected_width: %u\n", width, 17);
-        printf("height: %u, expected_height: %u\n", height, 5);
 
         return false;
     }
@@ -69,24 +70,74 @@ bool test_spiral_8()
      * 1 1 1 1 1 1 1 1
      */
 
-    int width, height;
-
     uint32_t * state;
 
     const uint32_t expected_state[] = {
         0xff,
-        0x18,
-        0xfb,
-        0x1a,
-        0xda,
-        0x5a,
-        0xdb,
-        0x18,
+        0x81,
+        0xbf,
+        0xa1,
+        0xad,
+        0xa5,
+        0xbd,
+        0x81,
         0xff
     };
 
-    read_pbm("tests/pbm/spiral_8_start.pbm", &state, &width, &height);
+    read_pbm("tests/pbm/spiral_8_start.pbm", &state, &state_width, &state_height);
 
+    state_size = state_height * 4;
+
+    if (state_width != 8 && state_height != 9)
+    {
+        printf("width: %u, expected_width: %u\n", state_width, 8);
+        printf("height: %u, expected_height: %u\n", state_height, 9);
+
+        return false;
+    }
+    else if (!states_equal(state, expected_state))
+    {
+        puts("state read:");
+        print_state(state);
+
+        puts("state expected:");
+        print_state(expected_state);
+
+        return false;
+    }
+
+    return true;
+}
+
+bool test_simple()
+{
+    /* State:
+     *
+     * |LSB MSB|
+     * 1 0 0 1 0
+     * 0 0 0 0 1
+     * 0 1 0 0 0
+     */
+
+    uint32_t * state;
+
+    const uint32_t expected_state[] = {
+        0x09,
+        0x10,
+        0x02
+    };
+
+    read_pbm("tests/pbm/simple_start.pbm", &state, &state_width, &state_height);
+
+    state_size = state_height * 4;
+
+    if (state_width != 5 && state_height != 3)
+    {
+        printf("width: %u, expected_width: %u\n", state_width, 8);
+        printf("height: %u, expected_height: %u\n", state_height, 9);
+
+        return false;
+    }
     if (!states_equal(state, expected_state))
     {
         puts("state read:");
@@ -97,21 +148,17 @@ bool test_spiral_8()
 
         return false;
     }
-    else if (width != 8 && height != 9)
-    {
-        printf("width: %u, expected_width: %u\n", width, 8);
-        printf("height: %u, expected_height: %u\n", height, 9);
-
-        return false;
-    }
 
     return true;
 }
 
 int main(int argc, char * argv[])
 {
+    ints_per_row = 1;
+
     RUN_TEST(test_61c);
     RUN_TEST(test_spiral_8);
+    RUN_TEST(test_simple);
 
     return EXIT_STATUS;
 }
